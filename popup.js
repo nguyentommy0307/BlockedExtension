194a154c2768ebda5ffb8e40b672865a0a49c8fa
nguyentommy0307 <nguyentommy315@g.ucla.edu>
2026-01-12 22:58:38 -0800

    Added javascript to save user input and sava data using Chrome storage api

--- popup.js
+++ popup.js
@@ -1,1 +1,29 @@
-console.log('Blocked MF')
+document.getElementById('blockBtn').addEventListener('click', () => {
+
+    const site = document.getElementById('website').value.trim();
+    const reason = document.getElementById('message').value.trim();
+
+
+    if (site === "") {
+        alert("Please enter a website domain to block!");
+        return;
+    }
+
+    if (reason === "") {
+        alert("Please give a reason to stay focused!");
+        return;
+    }
+
+    const dataToStore = {};
+    dataToStore[site] = reason;
+
+    chrome.storage.local.set(dataToStore, () => {
+        console.log("Success! Saved to storage.");
+
+        document.getElementById('website').value = "";
+        document.getElementById('message').value = "";
+
+        alert(`Added ${site} to your block list!`);
+    });
+
+});
